77361246875392181d2284b496889c366208774c
toastedwedge <88474004+toastedwedge@users.noreply.github.com>
2022-09-04 23:05:51 -0500

    Initial commit. Everything is a mess.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Shell in C
+
+Practice project
---
+++ main.c
@@ -0,0 +1,115 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define EXIT_SUCCESS    0
+#define EXIT_FAILURE    1
+#define LSH_RL_BUFSIZE  1024
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM   " \t\r\n\a"
+
+int lsh_execute(){
+    return 0;
+}
+
+char** lsh_split_line(char* line)
+{
+    int bufsize = LSH_TOK_BUFSIZE, position = 0;
+    char** tokens = malloc(bufsize * sizeof(char*));
+    char* token;
+
+    if (!tokens)
+    {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, LSH_TOK_DELIM);
+    while (token != NULL)
+    {
+        tokens[position] = token;
+        position++;
+
+    if (position >= bufsize)
+    {
+        bufsize += LSH_TOK_BUFSIZE;
+        tokens = realloc(tokens, bufsize * sizeof(char*));
+        if (!tokens)
+        {
+            fprintf(stderr, "lsh allocation error\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+        token = strtok(NULL, LSH_TOK_DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
+
+char* lsh_read_line(void)
+{
+    int bufsize = LSH_RL_BUFSIZE;
+    int position = 0;
+    char* buffer = malloc(sizeof(char) * bufsize);
+    int c;
+
+    if (!buffer)
+    {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1)
+    {
+        c = getchar();
+
+        if (EOF == c || '\n' == c)
+        {
+            buffer[position] = '\0';
+            return buffer;
+        }
+        else
+        {
+            buffer[position] = c;
+        }
+        position++;
+
+        if (position >= bufsize)
+        {
+            bufsize += LSH_RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if (!buffer)
+            {
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
+
+void lsh_loop(void)
+{
+    
+    char *line;
+    char **args;
+    int status;
+
+    do 
+    {
+        printf("> ");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+        status = lsh_execute(args);
+
+        free(line);
+        free(args);
+
+    } while (status);
+}
+
+int main(int argc, char **argv)
+{
+    lsh_loop();
+
+    return EXIT_SUCCESS;
+}
